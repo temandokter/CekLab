869e4512b39300d9b56cc63814f141998f5bff7f
DEDE SYARIFUDIN HIDAYAT <Mrz_dede@yahoo.co.id>
2019-05-27 13:48:32 +0700

    Kita Mulai Laravel

--- TemanDokter.php
+++ TemanDokter.php
@@ -1,2 +1,12 @@
 <h1>Test Teman Dokter</h1>
 <h1>Ada Perubahan, Silahkan Push Lagi</h1>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Kita Mulai Laravel</h1>
+</body>
+</html>
